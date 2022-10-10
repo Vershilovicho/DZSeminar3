a738f5951f5b93a1cf79b5b87dcc98fa83bb93a5
Vershilovicho <vernik281186@gmail.com>
2022-10-10 20:00:20 +0300

    Add Task23

---
+++ Task23/Program.cs
@@ -0,0 +1,29 @@
+﻿// Задача 23 
+// Напишите программу, которая принимает на вход число (N) и выдаёт таблицу кубов чисел от 1 до N. 
+// 5 -> 
+// 1 | 1 
+// 2 | 8 
+// 3 | 27 
+// 4 | 64 
+// 5 | 125 
+
+
+
+
+Console.WriteLine("Введите число N: ");
+int number = Convert.ToInt32(Console.ReadLine());
+
+CubeTable(number);
+void CubeTable(int num)
+
+{
+    int index = 1;
+    Console.WriteLine($"Таблица кубов чисел от 1 до {num}");
+    while (index <= num)
+    {
+        Console.WriteLine($"|{index,5}|{Math.Pow(index, 3),5}|");
+        index++;
+    }
+}
+
+
