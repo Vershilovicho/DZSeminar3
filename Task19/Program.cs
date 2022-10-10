38ba7f7dd055a727839122722d334c9f6cb771a4
Vershilovicho <vernik281186@gmail.com>
2022-10-10 21:00:23 +0300

    Add method to Task19

--- Task19/Program.cs
+++ Task19/Program.cs
@@ -7,26 +7,52 @@
 
 
 
+// Решение без метода:
+// Console.WriteLine("Введите пятизначное число: ");
+// int number = Convert.ToInt32(Console.ReadLine());
+
+// int num1 = number / 10000;
+// int num2 = (number / 1000) % 10;
+// int num4 = (number % 100) / 10;
+// int num5 = number % 10;
+
+// if (number > 9999 && number < 100000)
+
+//     if (num1 == num5 && num2 == num4)
+//     {
+//         Console.WriteLine($"Да, число {number} является палиндромом");
+//     }
+//     else
+//     {
+//         Console.WriteLine($"Нет, число {number} не является палиндромом");
+//     }
+// else
+//     {
+//         Console.WriteLine("Число не пятизначное");
+//     }
 
 Console.WriteLine("Введите пятизначное число: ");
 int number = Convert.ToInt32(Console.ReadLine());
 
-int num1 = number / 10000;
-int num2 = (number / 1000) % 10;
-int num4 = (number % 100) / 10;
-int num5 = number % 10;
-
-if (number > 9999 && number < 100000)
-
-    if (num1 == num5 && num2 == num4)
-    {
-        Console.WriteLine($"Да, число {number} является палиндромом");
-    }
+Palindrome (number);
+void Palindrome (int num)
+{
+    int num1 = num / 10000;
+    int num2 = (num / 1000) % 10;
+    int num4 = (num % 100) / 10;
+    int num5 = num % 10;
+    if (num > 9999 && num < 100000)
+        if (num1 == num5 && num2 == num4)
+        {
+            Console.WriteLine($"Да, число {number} является палиндромом");
+        }
+        else
+        {
+            Console.WriteLine($"Нет, число {number} не является палиндромом");
+        }
     else
-    {
-        Console.WriteLine($"Нет, число {number} не является палиндромом");
-    }
-else
-    {
-        Console.WriteLine("Число не пятизначное");
-    }
+        {
+            Console.WriteLine("Число не пятизначное");
+        }
+}
+
